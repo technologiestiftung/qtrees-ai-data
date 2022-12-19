b0ff0dff3e75b0a3315a70b7de01a3bc76c9dc29
Nicolas Gorges <nico@birdsonmars.com>
2022-12-19 14:11:48 +0100

    increase volume size; reust elastic ip

--- infrastructure/terraform/main.tf
+++ infrastructure/terraform/main.tf
@@ -149,6 +149,10 @@ resource "aws_instance" "qtrees" {
   ami           = "ami-065deacbcaac64cf2"
   instance_type = "t2.large"
 
+  root_block_device {
+    volume_size = 20
+  }
+
   # public ip needed to ssh into this instance
   associate_public_ip_address = true
   subnet_id                   = module.vpc.public_subnets[0]
@@ -161,12 +165,23 @@ resource "aws_instance" "qtrees" {
   }
 }
 
-resource "aws_eip" "qtrees" {
-  instance = aws_instance.qtrees.id
+# use this for generating a elastic ip on creation
+//resource "aws_eip" "qtrees" {
+//  instance = aws_instance.qtrees.id
+//
+//  tags = {
+//    Name = "${var.project_name}-iac-${var.qtrees_version}"
+//  }
+//}
 
-  tags = {
-    Name = "${var.project_name}-iac-${var.qtrees_version}"
-  }
+# re-use existing elastic ip
+data "aws_eip" "qtrees" {
+  public_ip = "${var.ELASTIC_IP_EC2}"
+}
+
+resource "aws_eip_association" "eip_assoc" {
+  instance_id   = aws_instance.qtrees.id
+  allocation_id = data.aws_eip.qtrees.id
 }
 
 # Export Terraform variable values to an Ansible var_file
@@ -175,7 +190,7 @@ resource "local_file" "tf_ansible_vars_file" {
     [qtrees_server:vars]
     ansible_ssh_private_key_file=${var.private_key}
     [qtrees_server]
-    ubuntu@${aws_eip.qtrees.public_dns}
+    ubuntu@${data.aws_eip.qtrees.public_dns}
     DOC
   filename = "./ansible/hosts"
 }
@@ -198,7 +213,7 @@ resource "local_file" "setup_env_file" {
 
 resource "local_file" "ssh_file" {
   content  = <<-DOC
-    ssh -i "~/.ssh/qtrees.pem" ubuntu@${aws_eip.qtrees.public_dns}
+    ssh -i "~/.ssh/qtrees.pem" ubuntu@${data.aws_eip.qtrees.public_dns}
     DOC
   filename = "./tf_output/ssh_to_ec2.sh"
 }
@@ -210,71 +225,3 @@ resource "local_file" "connect_to_db" {
     DOC
   filename = "./tf_output/connect_to_db.sh"
 }
-
-## ============================= LOAD BALANCING
-#resource "aws_lb" "qtrees" {
-#  name               = "${var.project_name}-lb"
-#  internal           = false
-#  load_balancer_type = "application"
-#  security_groups    = [aws_security_group.qtrees_lb_sg.id]
-#  subnets = module.vpc.public_subnets
-#
-#  tags = {
-#    Name = "${var.project_name}-iac-${var.qtrees_version}"
-#  }
-#}
-
-#resource "aws_security_group" "qtrees_lb_sg" {
-#  name   = "${var.project_name}_lb_sg"
-#  vpc_id = module.vpc.vpc_id
-#  ingress {
-#    description = "HTTP"
-#    from_port   = 80
-#    to_port     = 80
-#    protocol    = "tcp"
-#    cidr_blocks = ["0.0.0.0/0"]
-#  }
-#
-#  egress {
-#    from_port        = 0
-#    to_port          = 0
-#    protocol         = "-1"
-#    cidr_blocks      = ["0.0.0.0/0"]
-#    ipv6_cidr_blocks = ["::/0"]
-#  }
-#  tags = {
-#    Name = "${var.project_name}-iac-${var.qtrees_version}"
-#  }
-#}
-
-#resource "aws_lb_target_group" "qtrees_lb_tg" {
-#  name     = "${var.project_name}-lb-tg"
-#  port     = 80
-#  protocol = "HTTP"
-#  vpc_id   = module.vpc.vpc_id
-#
-#  tags = {
-#    Name = "${var.project_name}-iac-${var.qtrees_version}"
-#  }
-#}
-
-#resource "aws_lb_target_group_attachment" "target_instance" {
-#  target_group_arn = aws_lb_target_group.qtrees_lb_tg.arn
-#  target_id        = aws_instance.qtrees.id
-#  port             = 3000
-#}
-
-#resource "aws_lb_listener" "qtrees" {
-#  load_balancer_arn = aws_lb.qtrees.arn
-#  port              = "80"
-#  protocol          = "HTTP"
-
-#  default_action {
-#    type             = "forward"
-#    target_group_arn = aws_lb_target_group.qtrees_lb_tg.arn
-#  }
-#
-#  tags = {
-#    Name = "${var.project_name}-iac-${var.qtrees_version}"
-#  }
-#}
