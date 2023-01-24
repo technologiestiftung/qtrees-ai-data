e0105679364300f40062bf8639b1f24e26c8b6f4
Marcus <marcus@birdsonmars.com>
2023-01-24 12:35:35 +0100

    using rainfall function

--- infrastructure/database/templates/07_create_or_update_views.template.sql
+++ infrastructure/database/templates/07_create_or_update_views.template.sql
@@ -25,10 +25,10 @@ from public.trees as trees
 join public.radolan_tiles as tiles
 ON ST_Contains(tiles.geometry, trees.geometry);
 
-CREATE OR REPLACE VIEW public.radolan_14d_agg AS
-SELECT radolan.timestamp, radolan.tile_id,
-SUM(radolan.rainfall_mm) OVER (partition by radolan.tile_id ORDER BY radolan.timestamp ROWS BETWEEN 13 PRECEDING AND CURRENT ROW) AS rainfall_mm_14d_sum
-FROM public.radolan;
+--CREATE OR REPLACE VIEW public.radolan_14d_agg AS
+--SELECT radolan.timestamp, radolan.tile_id,
+--SUM(radolan.rainfall_mm) OVER (partition by radolan.tile_id ORDER BY radolan.timestamp ROWS BETWEEN 13 PRECEDING AND CURRENT ROW) AS rainfall_mm_14d_sum
+--FROM public.radolan;
 
 CREATE OR REPLACE VIEW private.training_data AS
 SELECT sensor_measurements.tree_id, sensor_measurements.sensor_id, sensor_measurements.timestamp, sensor_measurements.value,
@@ -39,6 +39,7 @@ FROM private.sensor_measurements
 LEFT JOIN public.shading_wide ON public.shading_wide.tree_id = sensor_measurements.tree_id
 LEFT JOIN public.trees ON trees.id = sensor_measurements.tree_id
 LEFT JOIN public.weather_14d_agg ON sensor_measurements.timestamp = weather_14d_agg.timestamp
+LEFT JOIN public.rainfall(sensor_measurements.tree_id) ON sensor_measurements.timestamp=public.rainfall.date
 ORDER BY timestamp DESC;
 
 CREATE OR REPLACE VIEW private.test_data AS
