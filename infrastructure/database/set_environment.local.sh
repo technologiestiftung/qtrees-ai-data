743c19527cdb3c4551a4a085ebbaccc4aefe97fe
Marcus Voss <89918821+birdMarcus@users.noreply.github.com>
2023-02-09 15:07:23 +0100

    added blank env variables

--- infrastructure/database/set_environment.local.sh
+++ infrastructure/database/set_environment.local.sh
@@ -9,3 +9,6 @@ export CMD_GIS_ADMIN="ALTER ROLE gis_admin SUPERUSER;" # local use
 export DB_ADMIN_PASSWD="adminadminadmin"
 export DB_USER_PASSWD="useruseruser"
 export UI_USER_PASSWD="uiuiuiuiui"
+
+export GDK_PASSWD=""
+export DB_GDK=""
