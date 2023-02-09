8f0e58cd84a87da9115858052d2dbfa9dd75f55e
Nicolas Gorges <nico@birdsonmars.com>
2023-02-09 16:18:22 +0100

    added watering view and table watering_sga; index for now- and forecast

--- infrastructure/database/templates/03_setup_tables.template.sql
+++ infrastructure/database/templates/03_setup_tables.template.sql
@@ -194,6 +194,11 @@ CREATE TABLE public.nowcast (
 	model_id text
 );
 
+CREATE INDEX idx_nowcast_tree_id
+ON nowcast(tree_id);
+CREATE INDEX idx_forecast_tree_id
+ON forecast(tree_id);
+
 insert into public.sensor_types(id, name) values (1, 'saugspannung_30cm');
 insert into public.sensor_types(id, name) values (2, 'saugspannung_60cm');
 insert into public.sensor_types(id, name) values (3, 'saugspannung_90cm');
@@ -235,6 +240,13 @@ CREATE TABLE private.sensor_measurements (
 );
 
 CREATE TABLE private.watering_gdk (
+    tree_id TEXT REFERENCES public.trees(id),
+    amount_liters REAL,
+    date DATE,
+    PRIMARY KEY(tree_id, date)
+);
+
+CREATE TABLE private.watering_sga (
     tree_id TEXT REFERENCES public.trees(id),
     amount_liters REAL,
     date DATE,
