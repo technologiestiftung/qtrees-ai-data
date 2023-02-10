462e8c7b98a5e2fb6f64f274955ceddb409d3673
Nicolas Gorges <nico@birdsonmars.com>
2023-02-10 15:47:51 +0100

    batchwise tree loading

--- qtrees/fisbroker.py
+++ qtrees/fisbroker.py
@@ -10,6 +10,111 @@ from qtrees.helper import get_logger
 logger = get_logger(__name__)
 
 
+def _prepare_tree_data(gdf, street_tree):
+    gdf = gdf.to_crs(4326)
+
+    gdf['street_tree'] = street_tree
+    gdf['baumscheibe'] = ""
+
+    gdf['lat'] = gdf.geometry.y
+    gdf['lng'] = gdf.geometry.x
+    # geopandas.to_file has problems with datetime
+    date = pd.to_datetime(datetime.now().date().strftime('%Y-%m-%d'))
+    gdf['created_at'] = date
+    gdf['updated_at'] = date
+    gdf = gdf.rename(columns={"baumid": "id"})
+    gdf = gdf.drop('gml_id', axis=1)
+    gdf.drop_duplicates(subset=['id'], keep='first')
+
+    return gdf
+
+
+def store_trees_batchwise_to_db(trees_file, street_tree, engine, lu_ids=None, n_batch_size=100000):
+    """
+    load tree file, process data, remove duplicates and stored it into a db - all batchwise
+
+    Parameters
+    ----------
+    trees_file: str
+        filename for tree file
+    street_tree: bool
+        defines if data contains street trees (or anlagen trees)
+    engine: db engine object
+    lu_ids: set | None
+        existing tree ids
+    n_batch_size: int
+        number of trees which processed at once
+
+    Returns
+    -------
+        set, with tree ids so far
+    """
+    n_start = 0
+    n_round = 0
+    lu_ids = lu_ids or set()
+    while True:
+        n_end = n_start + n_batch_size
+        gdf = gpd.read_file(trees_file, rows=slice(n_start, n_end))
+        n_rows = len(gdf)
+        # prepare data and remove patch-internal duplicates
+        gdf = _prepare_tree_data(gdf, street_tree=street_tree)
+        duplicates = lu_ids.intersection(gdf["id"])
+        n_duplicates = len(duplicates) + n_rows - len(gdf)
+        if n_duplicates > 0:
+            logger.warning("Found duplicates: %s", list(duplicates))
+        gdf = gdf[~gdf["id"].isin(duplicates)]
+        lu_ids.update(gdf["id"])
+
+        n_round += 1
+        logger.info("Writing into db - batch %s (%s)", n_round, len(gdf))
+        n_start = n_end
+
+        try:
+            gdf.to_postgis("trees", engine, if_exists="append", schema="public")
+        except Exception as e:
+            logger.error("Cannot write to db: %s", e)
+            exit(121)
+
+        # running out of data
+        if len(gdf) < n_batch_size - n_duplicates:
+            break
+
+    return lu_ids
+
+
+def download_tree_file(dir_data, type, use_cached=True):
+    """
+    download and store raw tree data
+
+    Parameters
+    ----------
+    dir_data: str
+        cache dir
+    type: str
+        defines tree dataset - currently 'wfs_baumbestand' or 'wfs_baumbestand_an'
+    use_cached: bool
+        defines if cached data is used or data should be downloaded again
+
+    Returns
+    -------
+        str, filename for raw tree data
+
+    """
+    trees_file = os.path.join(dir_data, f"{type}.xml")
+    if os.path.exists(trees_file) and use_cached:
+        return trees_file
+
+    logger.info("Downloading '%s' data", type)
+    params = dict(service="WFS", version="2.0.0", request='GetFeature', typeNames=f"fis:s_{type}",
+                  srsName="EPSG:25833")
+    url = f"http://fbinter.stadt-berlin.de/fb/wfs/data/senstadt/s_{type}"
+
+    r = requests.get(url, params=params)
+    with open(trees_file, 'wb') as f:
+        f.write(r.content)
+    return trees_file
+
+
 def get_trees(trees_file):
     if os.path.isdir(trees_file):
         logger.warning("%s is not a valid file.", trees_file)
@@ -50,7 +155,7 @@ def get_trees(trees_file):
         logger.debug("Saving trees geo data frames to %s.", trees_file)
         trees_gdf.to_file(trees_file, driver='GeoJSON')
 
-    trees_gdf['created_at'] = pd.to_datetime(trees_gdf['created_at'] )
+    trees_gdf['created_at'] = pd.to_datetime(trees_gdf['created_at'])
     trees_gdf['updated_at'] = pd.to_datetime(trees_gdf['updated_at'])
     return trees_gdf
 
