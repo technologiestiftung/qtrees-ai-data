d4be91091d245f3cde1d7cb4e348a7977ac37bba
birdLova <lova.wahlin@birdsonmars.com>
2023-06-13 17:51:32 +0200

    Add giz extention and change fix bug in view

--- infrastructure/database/templates/03_setup_tables.template.sql
+++ infrastructure/database/templates/03_setup_tables.template.sql
@@ -2,6 +2,7 @@ CREATE EXTENSION postgis;
 CREATE EXTENSION fuzzystrmatch;
 CREATE EXTENSION postgis_tiger_geocoder;
 CREATE EXTENSION postgis_topology;
+CREATE EXTENSION tablefunc;
 
 --
 CREATE TABLE public.trees (
