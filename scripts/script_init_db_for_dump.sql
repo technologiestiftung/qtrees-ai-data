89748c6e819ce5a6e527ca720288ff7fb76434e0
birdLova <lova.wahlin@birdsonmars.com>
2023-07-05 15:18:33 +0200

    Fix db dump restore

---
+++ scripts/script_init_db_for_dump.sql
@@ -0,0 +1,14 @@
+CREATE EXTENSION postgis;
+CREATE EXTENSION fuzzystrmatch;
+CREATE EXTENSION postgis_tiger_geocoder;
+CREATE EXTENSION postgis_topology;
+CREATE EXTENSION tablefunc;
+
+ALTER SCHEMA tiger OWNER TO gis_admin;
+ALTER SCHEMA tiger_data OWNER TO gis_admin;
+ALTER SCHEMA topology OWNER TO gis_admin;
+
+CREATE SCHEMA IF NOT EXISTS private;
+GRANT ALL ON SCHEMA private TO postgres;
+
+CREATE SCHEMA IF NOT EXISTS basic_auth;
--- scripts/script_restore_db_data.sh
+++ scripts/script_restore_db_data.sh
@@ -7,20 +7,24 @@ if [ -n "$QTREES_VERSION" ]; then
     data_dir="data/db/"$QTREES_VERSION
   fi
 
-  files=$(ls -t $data_dir/*.dump 2>/dev/null)
+  files=$(ls -t $data_dir/*.psql 2>/dev/null)
   if [ -z "$files" ]; then
     # no dump file found
     echo "Error: No suitable file found in $data_dir"
   else
     # get newest dump-file
-    filename=$(ls -t $data_dir/*.dump | head -1)
+    filename=$(ls -t $data_dir/*.psql | head -1)
     # dump data before truncating
     filename_safe="$data_dir/safe.dump"
-    PGPASSWORD=${POSTGRES_PASSWD} pg_dump --host $DB_QTREES --port 5432 --username postgres --format custom --file filename_safe -n public -n private --exclude-table public.spatial_ref_sys --data-only qtrees
-    echo "Truncating data in DB"
-    PGPASSWORD=${POSTGRES_PASSWD} psql --host $DB_QTREES -U postgres -d qtrees -c "SELECT * from private.truncate_tables()"
+    PGPASSWORD=${POSTGRES_PASSWD} pg_dump --host $DB_QTREES --port 5432 --username postgres --format custom --file $filename_safe -n public -n private --exclude-table public.spatial_ref_sys --data-only qtrees
+    # echo "Truncating data in DB"
+    echo "Drop db ..."
+    PGPASSWORD=${POSTGRES_PASSWD} psql --host $DB_QTREES -U postgres -c "DROP DATABASE qtrees WITH (FORCE);"
+    echo "Initialize db ..." 
+    PGPASSWORD=${POSTGRES_PASSWD} psql --host $DB_QTREES -U postgres -c "CREATE DATABASE qtrees OWNER postgres;"    
+    PGPASSWORD=${POSTGRES_PASSWD} psql --host=$DB_QTREES --port=5432 --username=postgres --dbname=qtrees -a -f scripts/init_db_for_dump.sql
     echo "Loading data into DB from \"$filename\""
-    PGPASSWORD=${POSTGRES_PASSWD} pg_restore --host $DB_QTREES --port 5432 --username postgres $filename --data-only --dbname=qtrees
+    {echo "SET session_replication_role = replica;"; gunzip < $filename; } | PGPASSWORD=${POSTGRES_PASSWD} psql --host $DB_QTREES -U postgres -d qtrees
   fi
 else
   echo "Error: Variable QTREES_VERSION is not set"
